118e76bfb89ebb1c9b35ffafd3b8960d649e96f0
Nabcellent <nabcellent.dev@gmail.com>
2022-07-22 11:04:03 +0300

    Adds an api response interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,11 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { Status } from './enums';
 
+export interface ApiResponse<T> {
+    status?: string;
+    data?: T;
+}
+
 export type ToastDataType = {
     type: 'success' | 'info' | 'warning' | 'danger';
     msg: string;
@@ -29,73 +34,48 @@ export type RouteType = {
     children: RouteChildType[]
 }
 
-export type StkCallback = {
-    id: string
-    amount: number
-    result_desc: string
-    checkout_request_id: string
-    created_at: string
+/** MODEL INTERFACES
+ * */
+
+interface Model {
+    id: number;
+    created_at: string;
 }
 
-export type StkRequest = {
-    id: string
-    checkout_request_id: string
-    amount: number
-    phone: number
-    reference: string
-    status: Status
-    created_at: string
-    response?: StkCallback
+export interface StkCallback extends Model {
+    amount: number;
+    result_desc: string;
+    checkout_request_id: string;
 }
 
-export type Voucher = {
-    id: number
-    type: string
-    balance: number
-    account_id: number
-    created_at: string
-    voucher_transactions?: VoucherTransaction[]
+export interface StkRequest extends Model {
+    checkout_request_id: string;
+    amount: number;
+    phone: number;
+    reference: string;
+    status: Status;
+    response?: StkCallback;
 }
 
-export type VoucherTransaction = {
-    id: number
-    type: string
-    amount: number
-    description: string
-    voucher_id: number
-    created_at: string
-    voucher?: Voucher
+export interface Voucher extends Model {
+    type: string;
+    balance: number;
+    account_id: number;
+    voucher_transactions?: VoucherTransaction[];
 }
 
-export type Payment = {
-    id: number
-    amount: number
-    type: string
-    subtype: string
-    status: Status
-    created_at: string
-    providable?: StkRequest | VoucherTransaction
+export interface VoucherTransaction extends Model {
+    type: string;
+    amount: number;
+    description: string;
+    voucher_id: number;
+    voucher?: Voucher;
 }
 
-export type TandaRequest = {
-    request_id: number
-    receipt_number: number
-    amount: number
-    provider: string
-    message: string
-    destination: string
-    last_modified: string
-    status: number
-}
-
-export type Transaction = {
-    id?: number
-    status: Status
-    description: string
-    destination: string
-    type: string
-    amount: number
-    created_at: string
-    payment?: Payment
-    request?: TandaRequest
+export interface Payment extends Model {
+    amount: number;
+    type: string;
+    subtype: string;
+    status: Status;
+    providable?: StkRequest | VoucherTransaction;
 }
