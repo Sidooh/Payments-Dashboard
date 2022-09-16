ca9ce8a30895e596a5ba1bb1eb89ff8e7b25653a
Nabcellent <nabcellent.dev@gmail.com>
2022-09-16 08:22:29 +0300

    Aligns dashboard summaries

--- src/pages/dashboards/default/statistics/summaries/PaymentSummaries.tsx
+++ src/pages/dashboards/default/statistics/summaries/PaymentSummaries.tsx
@@ -14,46 +14,44 @@ const PaymentSummaries = () => {
     logger.log(stats);
 
     return (
-        <>
-            <Row className="g-3 g-xxl-0 h-100">
-                <Col md={6} xxl={12} className={'mb-xxl-2'}>
-                    <Card className={'h-xl-100 d-flex'}>
-                        <CardBgCorner corner={2}/>
-                        <Card.Body as={Row}>
-                            <Col className="d-md-flex flex-column justify-content-center">
-                                <h5 className="mb-md-0 mb-lg-2">Payments</h5>
-                                <h4 className="fs-3 fw-normal text-700">
-                                    <CountUp end={stats.total_payments} separator=","/>
-                                </h4>
-                            </Col>
-                            <Col className={'d-flex align-items-start justify-content-end'}>
-                                <Badge pill>
-                                    <CountUp end={stats.total_payments_today} separator=","/>
-                                </Badge>
-                            </Col>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6} xxl={12} className={'mt-xxl-2'}>
-                    <Card className={'h-xl-100'}>
-                        <CardBgCorner/>
-                        <Card.Body as={Row}>
-                            <Col className="d-md-flex flex-column justify-content-center">
-                                <h5 className="mb-md-0 mb-lg-2">Revenue</h5>
-                                <h4 className="fs-3 fw-normal text-700 align-text-bottom">
-                                    <CountUp end={stats.total_revenue} prefix={'KES '} separator=","/>
-                                </h4>
-                            </Col>
-                            <Col className={'d-flex align-items-start justify-content-end'}>
-                                <Badge bg={'success'} pill>
-                                    <CountUp end={stats.total_revenue_today} prefix={'KES '} separator=","/>
-                                </Badge>
-                            </Col>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </>
+        <Row className="g-3 g-xxl-0 h-100">
+            <Col md={6} xxl={12} className={'mb-xxl-2'}>
+                <Card className={'h-xl-100 d-flex'}>
+                    <CardBgCorner corner={2}/>
+                    <Card.Body as={Row}>
+                        <Col className="d-md-flex flex-column justify-content-center">
+                            <h5 className="mb-md-0 mb-lg-2">Payments</h5>
+                            <h4 className="fs-3 fw-normal text-700">
+                                <CountUp end={stats.total_payments} separator=","/>
+                            </h4>
+                        </Col>
+                        <Col className={'d-flex align-items-start justify-content-end'}>
+                            <Badge pill>
+                                <CountUp end={stats.total_payments_today} separator=","/>
+                            </Badge>
+                        </Col>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col md={6} xxl={12} className={'mt-xxl-2'}>
+                <Card className={'h-xl-100'}>
+                    <CardBgCorner/>
+                    <Card.Body as={Row}>
+                        <Col className="d-md-flex flex-column justify-content-center">
+                            <h5 className="mb-md-0 mb-lg-2">Revenue</h5>
+                            <h4 className="fs-3 fw-normal text-700 align-text-bottom">
+                                <CountUp end={stats.total_revenue} prefix={'KES '} separator=","/>
+                            </h4>
+                        </Col>
+                        <Col className={'d-flex align-items-start justify-content-end'}>
+                            <Badge bg={'success'} pill>
+                                <CountUp end={stats.total_revenue_today} prefix={'KES '} separator=","/>
+                            </Badge>
+                        </Col>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     );
 };
 
