b46c45935da182f2306e04def3db0b03a011943b
Nabcellent <nabcellent.dev@gmail.com>
2023-01-31 13:20:59 +0300

    Updates sui-react && sidebar navigation

--- src/pages/dashboards/default/RecentPayments.tsx
+++ src/pages/dashboards/default/RecentPayments.tsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 
-const RecentPayments = ({payments}: { payments: Payment[] }) => {
+const RecentPayments = ({ payments }: { payments: Payment[] }) => {
     const navigate = useNavigate();
 
     return (
@@ -23,22 +23,22 @@ const RecentPayments = ({payments}: { payments: Payment[] }) => {
                     {
                         accessorKey: 'amount',
                         header: 'Amount',
-                        cell: ({row}: any) => currencyFormat(row.original.amount)
+                        cell: ({ row }: any) => currencyFormat(row.original.amount)
                     },
                     {
                         accessorKey: 'status',
                         header: 'Status',
-                        cell: ({row}: any) => <StatusChip status={row.original.status}/>
+                        cell: ({ row }: any) => <StatusChip status={row.original.status}/>
                     },
                     {
                         accessorKey: 'updated_at',
                         header: 'Date',
                         accessorFn: (row: Payment) => getRelativeDateAndTime(row.updated_at).toString(),
-                        cell: ({row}: any) => <TableDate date={row.original.updated_at ?? row.original.created_at}/>
+                        cell: ({ row }: any) => <TableDate date={row.original.updated_at ?? row.original.created_at}/>
                     },
                     {
                         id: 'Actions',
-                        cell: ({row}: any) => (
+                        cell: ({ row }: any) => (
                             <Link to={`/payments/${row.original.id}`}><FontAwesomeIcon icon={faEye}/></Link>
                         )
                     }
